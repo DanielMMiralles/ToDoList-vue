3c5514f43b06518a47f82fd44210a4b2282f2abc
Daniel <damm172004@gmail.com>
2024-12-14 18:07:43 -0400

    TodoList_ListItem-created

---
+++ src/components/TodoList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    import ListItem from './ListItem.vue';
+</script>
+
+<template>
+    <ul>
+        
+        <ListItem :is-Checked="false">
+            Esta en la tarea
+        </ListItem>
+    </ul>
+
+</template>
+
+<style>
+
+</style>
